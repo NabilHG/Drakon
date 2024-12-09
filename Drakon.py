b2e9d32ab4a3e64ba80d34bbc7e8c0ac3981bf32
NabilHG <nabilhajjoune@gmail.com>
2024-12-09 16:39:04 +0100

    done class Island

--- Drakon.py
+++ Drakon.py
@@ -1,20 +1,46 @@
+import numpy as np
+
 class Island():
     def __init__(self, r, d):
+        def create_board(r):
+            dim = 2 * r - 1  # dimension of the square matrix (2*r)-1
+            grid = np.full((dim, dim), -9)  # init the grid with -9
+            
+            for i in range(dim):
+                for j in range(dim):
+                    if abs(i - (r - 1)) + abs(j - (r - 1)) < r: # Manhattan distance to check is inside the rhombus shape
+                        grid[i, j] = 0
+            
+            return grid
+
+        
         self.grid = create_board(r)
         self.numdragons = d
 
+        print(self.grid)
 
-        def create_board(r): #square matrix, r + 2
-            pass
-
+    # get the value of a given coordinate                     
     def getCell(self, col, row):
         return self.grid[row,col]
 
+    # set the value of a given coordinate
     def setCell(self, col, row, v):
         self.grid[row,col] = v
 
+    # converting the grid to make it more appealing
     def __str__(self):
-        pass
+        content = ''
+        for i in range(self.grid.shape[0]):
+            for j in range(self.grid.shape[0]):
+                if self.grid[i, j] == -9:
+                    content += ' · '
+                elif self.grid[i, j] == -1:
+                    content += ' * '
+                else:
+                    content += ' 0 '
+            content += '\n\n'
+
+        return content
 
 
 
@@ -37,4 +63,7 @@ two grids and the result of the game).
 
 if __name__ == "__main__":
     letsPlayDrakon()
-
+    
+    island = Island(3, 5)
+    print(island)
+ 
